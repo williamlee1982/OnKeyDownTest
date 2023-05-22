173f0dc98209ba5a2b848b0a947cf66e3e4ecf3d
william <williamlee1982@163.com>
2023-05-22 11:10:26 +0800

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OnKeyDownTest;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OnKeyDownTest;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
